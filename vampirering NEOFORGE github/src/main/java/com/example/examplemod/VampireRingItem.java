916872c408ca9ef79aa464b4727cd6e3bfe61c4e
LazyTurtle <turtlelazy99@gmail.com>
2026-07-05 14:16:05 +0200

    Delete vampirering NEOFORGE github directory

--- vampirering NEOFORGE github/src/main/java/com/example/examplemod/VampireRingItem.java
+++
@@ -1,9 +0,0 @@
-package com.example.examplemod;
-
-import net.minecraft.world.item.Item;
-
-public class VampireRingItem extends Item {
-    public VampireRingItem(Properties properties) {
-        super(properties);
-    }
-}
